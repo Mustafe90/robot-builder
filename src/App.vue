2416fc748988348ebfec296447ed230b53936e02
Lil Safe <N/A>
2019-10-03 23:39:47 +0100

    Added images and a new vue component that utilises those images, updated the app component with this component to enable rending off it before we implement routing

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,20 @@
       </nav>
     </header>
     <main>
-      <HomePage/>
+      <RobotBuilder/>
     </main>
   </div>
 </template>
 
 <script>
 
-import HomePage from './components/HomePage.vue';
+import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'app',
   components: {
-    HomePage,
+    RobotBuilder,
   },
 };
 </script>
